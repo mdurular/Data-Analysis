9b32e5e98e445ec0542ad1c844f05232e19e6bc8
mdurular <mdrlr46@gmail.com>
2021-10-24 16:50:55 +0300

    files

---
+++ 5.Assignments/3.DAwSQL/SQL Assigment2.sql
@@ -0,0 +1,78 @@
+/*
+1. Conversion Rate
+Below you see a table of the actions of customers visiting the website by clicking on two different types of advertisements given by an E-Commerce company. 
+Write a query to return the conversion and cancellation rates for each Advertisement type.
+Actions:
+Visitor_ID	Adv_Type	Action
+1	A	Left
+2	A	Order
+3	B	Left
+4	A	Order
+5	A	Review
+6	A	Left
+7	B	Left
+8	B	Order
+9	B	Review
+10	A	Review
+
+
+Desired_Output:
+
+Adv_Type	Conversion_Rate
+A	0.33
+B	0.25
+
+
+*/
+-- 1. a.	Create above table (Actions) and insert values
+
+CREATE TABLE Actions 
+(
+Visitor_ID int,
+Adv_Type VARCHAR(10),
+Action_m VARCHAR(10),
+);
+	 INSERT INTO Actions (Visitor_ID, Adv_Type,Action_m)
+ VALUES 
+(1,'A','Left'),
+(2,'A','Order'),
+(3,'B','Left'),
+(4,'A','Order'),
+(5,'A','Review'),
+(6,'A','Left'),
+(7,'B','Left'),
+(8,'B','Order'),
+(9,'B','Review'),
+(10,'A','Review');
+
+--2. b.	Retrieve count of total Actions, and Orders for each Advertisement Type
+
+SELECT Adv_Type, Count(Action_m) As Num_Action, (SELECT Count(Action_m) FROM Actions WHERE Action_m = 'Order' AND Adv_Type = 'A' ) As Num_order 
+FROM Actions
+WHERE Adv_Type = 'A'
+GROUP BY Adv_Type
+
+UNION 
+
+SELECT Adv_Type, Count(Action_m) As Num_Action, (SELECT Count(Action_m) FROM Actions WHERE Action_m = 'Order' AND Adv_Type = 'B' ) As Num_order 
+FROM Actions
+WHERE Adv_Type = 'B'
+GROUP BY Adv_Type
+
+--3.Calculate Orders (Conversion) rates for each Advertisement Type by dividing by total count of actions casting as float by multiplying by 1.0.
+
+SELECT Adv_Type, 100/(Num_Action/Num_Order)*0.01 AS Conversion_Rate
+FROM  (SELECT Adv_Type, Count(Action_m) As Num_Action, (SELECT Count(Action_m) FROM Actions WHERE Action_m = 'Order' AND Adv_Type = 'A' ) As Num_order 
+FROM Actions
+WHERE Adv_Type = 'A'
+GROUP BY Adv_Type
+
+UNION 
+
+SELECT Adv_Type, Count(Action_m) As Num_Action, (SELECT Count(Action_m) FROM Actions WHERE Action_m = 'Order' AND Adv_Type = 'B' ) As Num_order 
+FROM Actions
+WHERE Adv_Type = 'B'
+GROUP BY Adv_Type
+) New_table
+
+
